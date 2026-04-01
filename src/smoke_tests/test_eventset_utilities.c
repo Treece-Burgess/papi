3d9442114e63876288a0cb8029a5673fda032f40
Treece-Burgess <tburges6@vols.utk.edu>
2026-03-31 17:24:43 -0700

    using test_eventset_utilities

--- src/smoke_tests/test_eventset_utilities.c
+++ src/smoke_tests/test_eventset_utilities.c
@@ -17,6 +17,9 @@
 
 int main( int argc, char **argv )
 {
+    int number_of_components = argc - 1;
+    char **component_names = argv + 1;
+
     int retval = PAPI_library_init( PAPI_VER_CURRENT );
     if ( retval != PAPI_VER_CURRENT ) {
         printf("ERROR: PAPI_library_init: %d: %s\n", retval, PAPI_strerror(retval));
@@ -24,29 +27,25 @@ int main( int argc, char **argv )
     }
 
     int i;
-    for (i = 1; i < argc; i++) {
+    for (i = 0; i < number_of_components; i++) {
         int EventSet = PAPI_NULL;
         CHECK_PAPI_CALLS( PAPI_create_eventset (&EventSet) );
         if (EventSet != 0) {
-            fprintf(stderr, "");
+            fprintf(stderr, "Failed.");
             exit(EXIT_FAILURE);
         }
 
-        printf("We have: %s\n", argv[i]);
-        int cidx = PAPI_get_component_index(argv[i]);
+        int cidx = PAPI_get_component_index( component_names[i] );
         if (cidx < 0) {
             fprintf(stderr, "Error in call to PAPI_get_component_index: %s\n", PAPI_strerror(cidx));
             exit(EXIT_FAILURE);
-        }   
+        }
 
         int eventCode = 0 | PAPI_NATIVE_MASK;
         int modifier = PAPI_ENUM_FIRST;
         CHECK_PAPI_CALLS( PAPI_enum_cmp_event(&eventCode, modifier, cidx) );
 
-        char eventName[PAPI_MAX_STR_LEN] = { 0 };
-        CHECK_PAPI_CALLS( PAPI_event_code_to_name(eventCode, eventName) );
-
-        CHECK_PAPI_CALLS( PAPI_add_named_event(EventSet, eventName) );
+        CHECK_PAPI_CALLS( PAPI_add_event(EventSet, eventCode) );
 
         int event, number_of_events = 0;
         CHECK_PAPI_CALLS( PAPI_list_events(EventSet, &event,  &number_of_events) );
@@ -57,20 +56,9 @@ int main( int argc, char **argv )
 
         CHECK_PAPI_CALLS( PAPI_list_events(EventSet, &event, &number_of_events) );
         if (event != eventCode) {
-            fprintf(stderr, "Eventcode (%x) from PAPI_list_events does not match eventcode (%x) from PAPI_enum_cmp_event.\n", eventCode, event);
+            fprintf(stderr, "Eventcode (%x) from PAPI_list_events does not match eventcode (%x) from PAPI_enum_cmp_event.\n", event, eventCode);
             exit(EXIT_FAILURE);
         }
-
-        // TODO: Do I want to break up into test_eventset_counting and test_eventset_utilities?
-
-        CHECK_PAPI_CALLS( PAPI_start(EventSet) );
-
-        // do work
-        sleep(3);
-
-        long long counter_value;
-        CHECK_PAPI_CALLS( PAPI_stop(EventSet, &counter_value) );
-        printf("%s from the component %s produced the value: %lld\n", eventName, argv[i], counter_value);
 
         CHECK_PAPI_CALLS( PAPI_cleanup_eventset(EventSet) );
         CHECK_PAPI_CALLS( PAPI_destroy_eventset(&EventSet) );
